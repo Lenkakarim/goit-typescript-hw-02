ec97e1c2fd2567b605d5230d7de3e1839d471255
Len4ik <karimovaolena@yahoo.com>
2025-05-23 19:17:26 +0300

    delete coments

--- src/ApiServise/apiServise.ts
+++ src/ApiServise/apiServise.ts
@@ -1,28 +1,7 @@
-// import axios from "axios";
-
-// const API_KEY =
-//   "cecBBLvDF58YbznI-icbcr3mKAwVaJdlM4oNZQlfnJ8";
-// axios.defaults.baseURL = "https://api.unsplash.com/";
-
-// export const fetchImages = async (query, page) => {
-//   const { data } = await axios.get("search/photos", {
-//     params: {
-//       client_id: API_KEY,
-//       query,
-//       page,
-//       per_page: 12,
-//     },
-//   });
-
-//   return {
-//     images: data.results,
-//     total_pages: data.total_pages,
-//   };
-// };
 import axios from "axios";
 
 const API_KEY =
-  "oNpGMpgaibADokunuTZQIjG2DkS1Gw5n_IACBzuK4z8";
+  "cecBBLvDF58YbznI-icbcr3mKAwVaJdlM4oNZQlfnJ8";
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
 export type ImageType = {
